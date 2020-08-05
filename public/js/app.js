558371555966632341f55a5f08361da15784eed2
suhaibShanaa <55164168+suhaibShanaa@users.noreply.github.com>
2020-08-05 14:44:42 +0300

    00

--- public/js/app.js
+++ public/js/app.js
@@ -49920,8 +49920,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\laravel\noorLocal\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel\noorLocal\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\xampp\htdocs\laravel\noorLocal\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\xampp\htdocs\laravel\noorLocal\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
