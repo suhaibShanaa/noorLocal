38af0f75989bd04240c52013aeeb165136eeff91
suhaibShanaa <55164168+suhaibShanaa@users.noreply.github.com>
2020-08-06 16:41:33 +0300

    user created and display by axios and progress bar and sweetalert stoped

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,35 +8,43 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-
-    <title>LaraStart | Starter Laravel Application</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+    <title>Noor Local</title>
     <link rel="stylesheet" href="/css/app.css">
+
+
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper" id="app">
 
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
-
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
         <!-- Left navbar links -->
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
             </li>
-
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="index3.html" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="#" class="nav-link">Contact</a>
+            </li>
         </ul>
 
         <!-- SEARCH FORM -->
-        <div class="input-group input-group-sm">
-            <input class="form-control form-control-navbar" @keyup="searchit" v-model="search" type="search" placeholder="Search" aria-label="Search">
-            <div class="input-group-append">
-                <button class="btn btn-navbar" @click="searchit">
-                    <i class="fa fa-search"></i>
-                </button>
+        <form class="form-inline ml-3">
+            <div class="input-group input-group-sm">
+                <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                    <button class="btn btn-navbar" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
             </div>
-        </div>
-
+        </form>
     </nav>
     <!-- /.navbar -->
 
@@ -46,7 +54,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <a href="index3.html" class="brand-link">
             <img src="./img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
-            <span class="brand-text font-weight-light">Lara Start</span>
+            <span class="brand-text font-weight-light">Noor Local</span>
         </a>
 
         <!-- Sidebar -->
@@ -57,10 +65,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     <img src="./img/profile.png" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">
-                        {{Auth::user()->name}}
-                        <p>{{Auth::user()->type}}</p>
-                    </a>
+                     {{ Auth::user()->name }}
                 </div>
             </div>
 
@@ -69,6 +74,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                     <!-- Add icons to the links using the .nav-icon class
                          with font-awesome or any other icon font library -->
+
 
                     <li class="nav-item">
                         <router-link to="/dashboard" class="nav-link">
@@ -80,35 +86,31 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </router-link>
                     </li>
 
-                    @can('isAdmin')
-                        <li class="nav-item has-treeview">
-                            <a href="#" class="nav-link">
-                                <i class="nav-icon fa fa-cog green"></i>
-                                <p>
-                                    Management
-                                    <i class="right fa fa-angle-left"></i>
-                                </p>
-                            </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <router-link to="/users" class="nav-link">
-                                        <i class="fas fa-users nav-icon"></i>
-                                        <p>Users</p>
-                                    </router-link>
-                                </li>
-
-                            </ul>
-                        </li>
-
-                        <li class="nav-item">
-                            <router-link to="/developer" class="nav-link">
-                                <i class="nav-icon fas fa-cogs"></i>
-                                <p>
-                                    Developer
-                                </p>
-                            </router-link>
-                        </li>
-                    @endcan
+                    <li class="nav-item has-treeview">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fa fa-cog green"></i>
+                            <p>
+                                Managment
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <router-link to="/users"  class="nav-link active">
+                                    <i class="fas fa-users nav-icon"></i>
+                                    <p>Users</p>
+                                </router-link>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/developer" class="nav-link">
+                            <i class="nav-icon fas fa-cogs"></i>
+                            <p>
+                                Developer
+                            </p>
+                        </router-link>
+                    </li>
                     <li class="nav-item">
                         <router-link to="/profile" class="nav-link">
                             <i class="nav-icon fas fa-user orange"></i>
@@ -141,19 +143,40 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <!-- for example router view -->
+                <router-view></router-view>
+                <!-- set progressbar -->
+                <vue-progress-bar></vue-progress-bar>
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
 
         <!-- Main content -->
         <div class="content">
             <div class="container-fluid">
-                <router-view></router-view>
-
-                <vue-progress-bar></vue-progress-bar>
+                <div class="row">
+                    <div class="col-lg-6">
+                    </div>
+                </div>
+                <!-- /.row -->
             </div><!-- /.container-fluid -->
         </div>
         <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
 
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+        <div class="p-3">
+            <h5>Title</h5>
+            <p>Sidebar content</p>
+        </div>
+    </aside>
+    <!-- /.control-sidebar -->
 
     <!-- Main Footer -->
     <footer class="main-footer">
@@ -162,17 +185,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
             Anything you want
         </div>
         <!-- Default to the left -->
-        <strong>Copyright &copy; 2014-2018 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
     </footer>
 </div>
 <!-- ./wrapper -->
 
-@auth
-    <script>
-        window.user = @json(auth()->user())
-    </script>
-@endauth
+<!-- REQUIRED SCRIPTS -->
 
 <script src="/js/app.js"></script>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,3 +8,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('invoice', function(){
+    return view('invoice');
+});
+
+//Route::get('{path}','HomeController@index')->where( 'path', '([A-z\d-/_.]+)?' );
